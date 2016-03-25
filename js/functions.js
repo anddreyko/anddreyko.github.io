8990360c78f4a88d477405daab3234759f5a946e
anddreyko <anddreyko@mail.ru>
2016-03-25 19:56:43 +0300

    added mask

--- js/functions.js
+++ js/functions.js
@@ -1,59 +1,18 @@
-var pContainerHeight = $('.bird-box').height();
-
-$(window).scroll(function(){
-
-  var wScroll = $(this).scrollTop();
-  
-  if (wScroll <= pContainerHeight) {
-
-    $('.logo').css({
-      'transform' : 'translate(0, '+ wScroll /2 +'%)'
+$(function(){
+    var pContainerHeight = $('.frame').height();
+    var wScroll = 0;
+    $(window).scroll(function(){
+        wScroll = $(this).scrollTop();
+        if (wScroll <= pContainerHeight) {
+            $('#l1').css({
+              'transform' : 'translate(0, -'+ wScroll /40 +'%)'
+            });
+            $('#l11').css({
+              'transform' : 'translate(0, -'+ wScroll /40 +'%)'
+            });
+            $('#l2').css({
+              'transform' : 'translate('+ wScroll /5 +'%, 0)'
+            });
+        }
     });
-
-    $('.back-bird').css({
-      'transform' : 'translate(0, '+ wScroll /4 +'%)'
-    });
-
-    $('.fore-bird').css({
-      'transform' : 'translate(0, -'+ wScroll /40 +'%)'
-    });
-
-  }
-
-  // Landing Elements
-  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
-
-    $('.clothes-pics figure').each(function(i){
-
-      setTimeout(function(){
-        $('.clothes-pics figure').eq(i).addClass('is-showing');
-      }, (700 * (Math.exp(i * 0.14))) - 700);
-    });
-
-  }
-
-
-  // Promoscope
-  if(wScroll > $('.large-window').offset().top - $(window).height()){
-
-    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
-
-    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
-
-    $('.window-tint').css({'opacity': opacity});
-
-  }
-
-
-  // Floating Elements
-
-  if(wScroll > $('.blog-posts').offset().top - $(window).height()){
-
-    var offset = (Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350)).toFixed();
-
-    $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
-
-    $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
-
-  }
 });
