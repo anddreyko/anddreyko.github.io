55050de9caba190ab2472d000840fd59512df1ce
anddreyko <anddreyko@mail.ru>
2016-03-26 10:59:06 +0300

    added text

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,7 @@ $(function(){
         if (wScroll <= pContainerHeight) {
             $('.l40').attr('y', -50-wScroll/40 +'%');
             $('.l42').attr('y', '-'+ wScroll/40 +'%');
+            $('.l153').attr('x', 50-wScroll/20 +'%');
             $('.l41').attr('x', wScroll/10 +'%');
             console.log(wScroll);
         }
