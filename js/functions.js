13528ae533091bef875a4fd9c94444cf65e0d986
anddreyko <anddreyko@mail.ru>
2016-03-27 14:43:21 +0300

    first frame begin

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,33 @@
 $(function(){
-    var pContainerHeight = $('.frame').height();
-    var wScroll = 0;
-    $(window).scroll(function(){
+    var frame = $('.frame')
+      , frameHeight = frame.height()
+      , frameTop = frame.offset().top;
+    var wScroll = 0
+      , screen = $(window)
+      , w = screen.width()
+      , h = screen.height()
+      , k = w/1200;
+    frame
+        .attr('width'
+          , w)
+        .attr('height'
+          , frame.attr('height')*k);
+    /*
+    screen.scroll(function(){
         wScroll = $(this).scrollTop();
-        if (wScroll <= pContainerHeight) {
-            $('.l40').attr('y', -50-wScroll/40 +'%');
-            $('.l42').attr('y', '-'+ wScroll/40 +'%');
-            $('.l153').attr('x', 50-wScroll/20 +'%');
-            $('.l41').attr('x', wScroll/10 +'%');
-            console.log(wScroll);
-        }
+        //обход списка фреймов
+        //animate(frame);
     });
+    function animate(frame){
+        var frameHeight = frame.height()
+          , frameTop = frame.offset().top;
+        if (wScroll <= frameHeight+frameTop
+         && wScroll+h >= frameTop) {
+            $('.l40').attr('y', -50-(wScroll-frameTop)/40 +'%');
+            $('.l42').attr('y', -(wScroll-frameTop)/40 +'%');
+            $('.l153').attr('x', 50-(wScroll-frameTop)/20 +'%');
+            $('.l41').attr('x', (wScroll-frameTop)/10 +'%');
+            console.log(frameTop);
+        }
+    }*/
 });
