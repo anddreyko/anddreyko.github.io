4dea5f46ffbc7fc829d48ecf2bda37763d2be9da
anddreyko <anddreyko@mail.ru>
2016-03-27 22:50:02 +0300

    animated 1st frame

--- js/functions.js
+++ js/functions.js
@@ -1,44 +1,47 @@
 $(function(){
     var frame = $('.frame')
-      , frameHeight = frame.height()
-      , frameTop = frame.offset().top
       , wScroll = 0
+      , frameTop = 0
+      , frameHeight = 0
       , screen = $(window)
       , w = screen.width()
-      , h = screen.height()
+      , h = 0
       , k = 1900/w;
     resized();
     
     screen.resize(function(){
-        w = screen.width();
-        h = screen.height();
         resized();
     });
     
     screen.scroll(function(){
         wScroll = $(this).scrollTop();
         //обход списка фреймов
-        //animate(frame);
+        animate(frame);
     });
     
     function resized(){
+        w = screen.width();
+        h = screen.height();
         frame
             .attr('width'
               , w)
             .attr('height'
-              , frame.attr('height')/k)
+              , frame.attr('height')/k);
+             
+        frameTop = frame.offset().top;
+        frameHeight = frame.height();
     }
     
     function animate(frame){
         var frameHeight = frame.height()
           , frameTop = frame.offset().top;
-        if (wScroll <= frameHeight+frameTop
-         && wScroll+h >= frameTop) {
-            $('.l40').attr('y', -50-(wScroll-frameTop)/40 +'%');
-            $('.l42').attr('y', -(wScroll-frameTop)/40 +'%');
-            $('.l153').attr('x', 50-(wScroll-frameTop)/20 +'%');
-            $('.l41').attr('x', (wScroll-frameTop)/10 +'%');
-            console.log(frameTop);
+            console.log((wScroll+h)/k);
+        if (wScroll+h >= 1250/k) {                              //from 1250 to (2250-screenHeight)
+            $('.l1').attr('y', (wScroll+h) +'px');              //to 2250
+            $('.l2').attr('y', (wScroll+h)/1.1 +'px');          //from 785px to 1315px = 530px
+            
+            $('.l3').attr('y', (wScroll+h)/1.3 +'px');
+            $('.l4').attr('y', (wScroll+h)/2 +'px');/**/
         }
     }
 });
