1b7373a4f9be4350318e4a6956711f13c0c0c706
anddreyko <anddreyko@gmail.com>
2023-11-01 02:39:00 +0300

    feat (common): add minifiers

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,37 @@
+const CleanCSS = require('clean-css')
+const postcss = require('postcss')
+const autoprefixer = require('autoprefixer')
+const htmlmin = require('html-minifier')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('style')
   eleventyConfig.addPassthroughCopy({ 'files/fav': '/' })
+  eleventyConfig.addPassthroughCopy({ 'files/images': '/images' })
 
   eleventyConfig.addFilter('is_string', (value) => typeof value === 'string')
 
   eleventyConfig.addFilter('is_object', (value) => typeof value === 'object')
+
+  eleventyConfig.addFilter('cssmin',
+    (code) => new CleanCSS({}).minify(code).styles)
+
+  eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
+    postcss([autoprefixer()])
+      .process(cssCode, { from: undefined })
+      .then((r) => done(null, r.css), (e) => done(e, null))
+  })
+
+  eleventyConfig.addTransform('htmlmin', (value, outputPath) => {
+    if (outputPath.indexOf('.html') > -1) {
+      return htmlmin.minify(value, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+      })
+    }
+    return value
+  })
 
   return {
     dir: {
